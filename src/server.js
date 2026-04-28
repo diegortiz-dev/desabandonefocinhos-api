ac691f11658931ae469ae7ca47b68bf82eb7fc24
diegortiz-dev <ortizdtz@gmail.com>
2026-04-27 23:36:07 -0300

    cors para ser possivel conectar entre render e vercel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
+
+const cors = require('cors');
 const app = require('./app');
 const prisma = require('./config/prisma');
+
+// 🔥 CORS (antes das rotas)
+app.use(cors({
+  origin: 'https://desabandonefocinhos-front.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+// 🔥 Preflight (evita erro em algumas requisições)
+app.options('*', cors());
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +26,7 @@ const startServer = async () => {
     }
 
     app.listen(PORT, () => {
-      console.log(`✅ Backend rodando em: http://localhost:${PORT}`);
+      console.log(`✅ Backend rodando na porta: ${PORT}`);
     });
   } catch (error) {
     console.error('❌ Falha ao iniciar o servidor:', error);
